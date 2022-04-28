e26c983e7bfa416da8ef198c6164a2075646a871
Ethem <ethmtrgt@gmail.com>
2022-04-28 03:46:30 +0300

    feat: add dark mode toggle

--- src/events.js
+++ src/events.js
@@ -1,4 +1,4 @@
-
+document.querySelector('h2').addEventListener('click', () => {document.body.classList.toggle('dark')});
 document.querySelector('#button').addEventListener('click', redirectWhatsapp);
 document.querySelector('#phone').addEventListener('keypress', (event) => {
     if (event.keyCode === 13) {
