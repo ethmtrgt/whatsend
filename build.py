e194d5c453786bce9bf1a7284afe138c388fd717
Ethem <ethmtrgt@gmail.com>
2023-09-10 23:37:59 +0300

    feat: create build directory if not exists

--- build.py
+++ build.py
@@ -5,6 +5,9 @@ manifest = json.load(open('manifest.json'))
 name = manifest['name']
 version = manifest['version']
 
+if not os.path.isdir('build'):
+    os.mkdir('build')
+
 filename = f'{name}-{version}.zip'
 os.system(f"zip -r -FS build/{filename} * \
         --exclude '*.git*' \
